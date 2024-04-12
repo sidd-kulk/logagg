0bf49f5f57d00b091cf90a21eac4ae148313071e
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2024-04-12 21:51:51 +0530

    Refactoring GET method

--- src/main/java/com/logagg/springboot/handler/LogEntryController.java
+++ src/main/java/com/logagg/springboot/handler/LogEntryController.java
@@ -3,12 +3,9 @@ package com.logagg.springboot.handler;
 import com.logagg.springboot.repository.LogEntry;
 import com.logagg.springboot.repository.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 public class LogEntryController {
@@ -17,14 +14,14 @@ public class LogEntryController {
     private LogRepository logRepository;
 
     @PostMapping("/log")
-    public void log(@RequestBody LogEntry logEntry){
+    public void log(@RequestBody LogEntry logEntry) {
         System.out.println(logEntry);
         logRepository.save(logEntry);
     }
 
     @GetMapping("/log")
-    public @ResponseBody List<LogEntry> log() {
-        return StreamSupport.stream(logRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
+    @ResponseBody
+    public List<LogEntry> log() {
+        return logRepository.findAll();
     }
 }
--- src/main/java/com/logagg/springboot/repository/LogRepository.java
+++ src/main/java/com/logagg/springboot/repository/LogRepository.java
@@ -3,6 +3,11 @@ package com.logagg.springboot.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LogRepository extends CrudRepository<LogEntry, Long> {
+    @Override
+    List<LogEntry> findAll();
+
 }
